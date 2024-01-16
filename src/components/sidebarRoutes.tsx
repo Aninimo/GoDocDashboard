7ddf7b030947f0235c3510ac5569282b77cad358
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2024-01-16 10:04:11 -0200

    Create sidebarRoutes.tsx

---
+++ src/components/sidebarRoutes.tsx
@@ -0,0 +1,48 @@
+import { CalendarDays, Home, Settings, ScrollText ,UsersRound  } from 'lucide-react'
+
+import { SidebarItem } from './sidebarItem'
+
+const guestRoutes = [
+  {
+    icon: Home,
+    label: 'Home',
+    href: '/'
+  },
+  {
+    icon: UsersRound,
+    label: 'Patients',
+    href: '/patients'
+  },
+  {
+    icon: CalendarDays,
+    label: 'Appointments',
+    href: '/appointments'
+  },
+  {
+    icon: ScrollText,
+    label: 'Notes',
+    href: '/notes'
+  },
+  {
+    icon: Settings,
+    label: 'Settings',
+    href: '/user-profile'
+  }
+]
+
+export function SidebarRoutes(){
+  const routes = guestRoutes
+
+  return(
+    <div className='flex flex-col w-full'>
+      {routes.map((route) => (
+        <SidebarItem
+          key={route.href}
+          icon={route.icon}
+          label={route.label}
+          href={route.href}
+        />
+      ))}
+    </div>
+  )
+}
